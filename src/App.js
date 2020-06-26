ac48e523da794bd53053bc0281d5d1add09b88f4
Morgan Lee Kestner <morgankestner@Morgans-MacBook-Air.local>
2020-06-26 16:02:35 -0400

    #5 video

--- src/App.js
+++ src/App.js
@@ -11,9 +11,26 @@ const App = () => {
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
       <Toggle />
-      <input type="text" onChange={(e) => setName(e.target.value)} value={name} />
+      <form onSubmit  = {(e) => {
+        e.preventDefault();
+        formSubmit(name, setName)
+      }}
+      > 
+        <input
+          type="text"
+          onChange={(e) => setName(e.target.value)} 
+          value={name}
+        />
+        <button>Submit</button>
+      </form>
     </div>
   );
 };
+
+// generic parameters
+const formSubmit = (value, setValue) => {
+  console.log('email sent', value );
+  setValue('');
+}
 
 export default App;

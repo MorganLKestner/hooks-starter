2fbfa092d8d9dff5ac18d4e76a1a64e9a39b274b
Morgan Lee Kestner <morgankestner@Morgans-MacBook-Air.local>
2020-06-26 15:46:26 -0400

    #4 video

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import  Toggle from './Toggle'
 // function based components can use hooks
 // class components are different
 const App = () => {
@@ -10,6 +10,7 @@ const App = () => {
   return (
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
+      <Toggle />
       <input type="text" onChange={(e) => setName(e.target.value)} value={name} />
     </div>
   );

22e80c3e5bd605550e427f01fbf8f713475c83ad
Morgan Lee Kestner <morgankestner@Morgans-MacBook-Air.local>
2020-06-26 18:33:33 -0400

    #7 video useRef hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef  } from 'react';
 import  Toggle from './Toggle';
 import  { useTitleInput } from './hooks/useTitleInput';
 
@@ -6,15 +6,29 @@ import  { useTitleInput } from './hooks/useTitleInput';
 /*
 useEffect a fucntion runs after every completed render
 function based components can use hooks
-class components are different 
+class components are different
+
+useState
+useEffect
+useRef
+- reference a dom node itself 
+ref={ref}
+- can use thise with onclicks, and add a class 
+- using a ref gives you full access of that dom element
+I.e: 
+- useOnScreen
+- useOnClickOutside
+- useOnScroll
 */
 
 const App = () => {
 
   const [name, setName] = useTitleInput ('');
+  const ref = useRef();
+  console.log('ref:', ref.current)
 
   return (
-    <div className="main-wrapper">
+    <div className="main-wrapper" ref= {ref}>
       <h1>Level Up Dishes</h1>
       <Toggle />
       <form onSubmit  = {(e) => {

ec8c45914a0dcf038dd992cdb1ecc8117468814f
Morgan Lee Kestner <morgankestner@Morgans-MacBook-Air.local>
2020-06-26 15:32:57 -0400

    #3 video

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+// function based components can use hooks
+// class components are different
 const App = () => {
+
+  // value, setval is a function
+  const [name, setName] = useState('');
+
   return (
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
+      <input type="text" onChange={(e) => setName(e.target.value)} value={name} />
     </div>
   );
 };

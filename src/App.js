cf04ad28ae11cb0404e369ee3cefae34ad9721f3
MorganLKestner <morganleekestner@gmail.com>
2020-07-05 18:32:23 -0400

    video 12 fetching data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,57 @@
-import React, { useState, useEffect, useRef, } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import  Toggle from './Toggle';
 import  { useTitleInput } from './hooks/useTitleInput';
+
+const App = () => {
+
+  const [name, setName] = useTitleInput('');
+  const ref = useRef();
+
+  const [dishes, setDishes] = useState([]);
+
+  const fetchDishes = async () => {
+    const res = await fetch('https://my-json-server.typicode.com/leveluptuts/fakeapi/dishes');
+    const data = await res.json();
+    setDishes(data);
+  }
+
+  useEffect( () => {
+
+  })
+
+  return (
+    <div className="main-wrapper" ref= {ref}>
+      <h1>Level Up Dishes</h1>
+      <Toggle />
+      <form onSubmit  = {(e) => {
+        e.preventDefault();    
+      }}
+      > 
+        <input
+          type="text"
+          onChange={(e) => setName(e.target.value)} 
+          value={name}
+        />
+        <button>Submit</button>
+      </form>
+      {dishes.map(dish => (
+        <article className="dish-card dish-card--withImage">
+          <h3>{dish.name}</h3>
+          <p>{dish.desc}</p>
+          <div className='ingredients'>
+            {dish.ingredients.map(ingredient => (
+              <span>
+                {ingredient}
+              </span>
+            ))}
+          </div>
+        </article>
+      ))}
+    </div>
+  );
+};
+
+export default App;
 
 
 // Notes:
@@ -22,41 +73,3 @@ I.e:
 - useOnClickOutside
 - useOnScroll
 */
-
-
-const App = () => {
-
-  const [name, setName] = useTitleInput ('');
-  const ref = useRef();
-
-  const reverseWord = word => {
-    console.log ('called')
-    return word
-    .split("")
-    .reverse()
-    .join("");
-  }
-
-  const TitleReversed = reverseWord('Level Up Dishes')
-
-  return (
-    <div className="main-wrapper" ref= {ref}>
-      <h1>{TitleReversed}</h1>
-      {/* <h1>Level Up Dishes</h1> */}
-      <Toggle />
-      <form onSubmit  = {(e) => {
-        e.preventDefault();    
-      }}
-      > 
-        <input
-          type="text"
-          onChange={(e) => setName(e.target.value)} 
-          value={name}
-        />
-        <button>Submit</button>
-      </form>
-    </div>
-  );
-};
-
-export default App;

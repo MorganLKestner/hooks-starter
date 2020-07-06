beb14dd104f6776ab6bc603bda59b24febfd4785
MorganLKestner <morganleekestner@gmail.com>
2020-07-06 08:50:27 -0400

    17 video cleanup hooks

--- src/hooks/BodyScrollLock.js
+++ src/hooks/BodyScrollLock.js
@@ -2,8 +2,20 @@ import { useLayoutEffect } from 'react';
 
 function useBodyScrollLock() {
   useLayoutEffect(() => {
+    const orginalOverflow = window.getComputedStyle(document.body).overflow;
     document.body.style.overflow = 'hidden';
-  })
+    
+    // You can do this do
+    // this is how you remove or destory the effects
+    // did component unmount
+    //return function cleanup() {
+    return () => {
+      document.body.style.overflow = orginalOverflow;
+    }
+
+    // the array [] ensures it only gets run once
+    // only fires on mount and the return runs on unmount once
+  }, [])
 }
 
 export { useBodyScrollLock }

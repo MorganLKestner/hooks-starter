75a829beb7f4ecdbbbbe1766d9f0b1654c0009d8
MorganLKestner <morganleekestner@gmail.com>
2020-07-06 08:42:56 -0400

    16 video usebodyscrolllock

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,12 +1,12 @@
 import React, { useState, } from "react";
-
+import DishForm from './dishForm'
 
 const Toggle = () => {
   const [isToggled, setToggle] = useState(false);
   return ( 
     <div>
       <button onClick={() => {setToggle(!isToggled)}}>Toggle</button>
-      {isToggled && <h2>hello</h2>}
+      {isToggled && <DishForm />}
     </div>
   )
 }
